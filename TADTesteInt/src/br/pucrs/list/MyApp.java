776166d415dc5127ce89d83c7e139a692c9a4273
DESKTOP-3G3KIBU\mcmor <michael.c.mora@gmail.com>
2018-07-18 18:07:41 -0300

    Agora com MyApp para rodar.

---
+++ TADTesteInt/src/br/pucrs/list/MyApp.java
@@ -0,0 +1,12 @@
+package br.pucrs.list;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		ListTAD<String> mList = new ListSimpleLinked<>();
+		
+		System.out.println("***** Compilou e rodou!!! *****");
+
+	}
+
+}
